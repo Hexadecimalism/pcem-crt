ef7d1fa8a46b80578bfc6f9fc040ac462b1571c8
Michał Siejak <michal@siejak.pl>
2021-09-09 22:06:01 +0200

    Convert built-in crtemu image frame to png resource to reduce binary size

--- src/wx-sdl2-video-crt.c
+++ src/wx-sdl2-video-crt.c
@@ -9,11 +9,11 @@
 #include "ibm.h"
 #include "video.h"
 #include "wx-sdl2-video.h"
+#include "wx-utils.h"
 
 #define CRTEMU_PC_IMPLEMENTATION
 #define CRTEMU_PC_SDL
 #include "crtemu/crtemu_pc.h"
-#include "crtemu/crt_frame_pc.h"
 
 static PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers;
 static PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer;
@@ -30,6 +30,25 @@ static int last_video_h = 0;
 extern int video_vsync;
 
 int crt_monitor_frame = 1;
+
+static void crt_load_frame(void* img)
+{
+        int frame_w, frame_h;
+        wx_image_get_size(img, &frame_w, &frame_h);
+        const unsigned char* frame_rgb = wx_image_get_data(img);
+        const unsigned char* frame_alpha = wx_image_get_alpha(img);
+
+        unsigned char* frame_rgba = malloc(frame_w * frame_h * 4);
+        for(int i=0; i<frame_w*frame_h; ++i)
+        {
+                frame_rgba[4*i+0] = frame_rgb[3*i+0];
+                frame_rgba[4*i+1] = frame_rgb[3*i+1];
+                frame_rgba[4*i+2] = frame_rgb[3*i+2];
+                frame_rgba[4*i+3] = frame_alpha[i];
+        }
+        crtemu_pc_frame(crtemu, (unsigned int*)frame_rgba, frame_w, frame_h);
+        free(frame_rgba);
+}
 
 int crt_init(SDL_Window* window, sdl_render_driver requested_render_driver, SDL_Rect screen)
 {
@@ -60,7 +79,13 @@ int crt_init(SDL_Window* window, sdl_render_driver requested_render_driver, SDL_
                 pclog("Could not create crtemu context.\n");
                 return SDL_FALSE;
         }
-        crtemu_pc_frame(crtemu, (unsigned int*)a_crt_frame, 1024, 1024);
+
+        void* crtemu_frame = wx_image_load_resource("BITMAP_CRT_FRAME");
+        if(crtemu_frame)
+        {
+                crt_load_frame(crtemu_frame);
+                wx_image_free(crtemu_frame);
+        }
 
         glGenTextures(1, &screen_tex);
         glBindTexture(GL_TEXTURE_2D, screen_tex);
